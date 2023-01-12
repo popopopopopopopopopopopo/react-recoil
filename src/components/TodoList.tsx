bdb870e0275a5b0f3a78a879da68170e4fa773c9
kosun <temparamental@hotmail.com>
2023-01-12 15:19:12 +0900

    itemの追加、stateの更新処理の追加

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,35 @@
-import {atom, selector, useRecoilRefresher_UNSTABLE, useRecoilValue} from 'recoil';
+import { useState} from "react";
+import { useRecoilValue, useRecoilState } from 'recoil';
 import { todoListState } from "./TodoList.atom";
 import { todoListStatesCounter } from "./TodoList.selector";
 import TodoListCounter from "./TodoListCounter";
 
+let id = 1;
+const _getId = () => {
+    return id++;
+}
+
 function TodoList() {
-    const todoList = useRecoilValue(todoListState);
+    const [title, setTitle] = useState('');
+    const [todoList, setTodoList] = useRecoilState(todoListState);
     const totalNum = useRecoilValue(todoListStatesCounter)
+
+    const handleChange = (e:any) => {
+        setTitle(e.target.value);
+    };
+
+    const addItem = (e: any) => {
+        setTodoList((oldTodoList) => [
+            ...oldTodoList,
+            {
+                id: _getId(),
+                title: title,
+                isComplete: false,
+            },
+        ]);
+        setTitle('');
+    };
+
     console.log(todoList);
     return (
         <>
@@ -14,6 +38,10 @@ function TodoList() {
                 <li>Todoの登録数:{totalNum}</li>
             </ul>
             <TodoListCounter />
+            <div style={{ margin: '1em 0' }}>
+                <input type="text" value={title} onChange={handleChange} />
+                <button onClick={addItem}>Add</button>
+            </div>
             {todoList.map((item) => (
                 <div key={item.id}>{item.title}</div>
             ))}
